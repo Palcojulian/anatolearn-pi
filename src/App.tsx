baf08c45a119999426cd676c35c0727db24b130c
palcojulian <palcojulian@outlook.com>
2025-04-13 00:49:04 -0500

    style: :art: Agrega informacion quemada previsualizacion clases tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <h1>Explora - Aprende - Late</h1>
+      <h1>Explora - Aprende - Late</h1>
+      <h2>Explora - Aprende - Late</h2>
+      <h3>Explora - Aprende - Late</h3>
+      <h4>Explora - Aprende - Late</h4>
+      <h5>Explora - Aprende - Late</h5>
+      <h6>Explora - Aprende - Late</h6>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis iusto
+        magnam optio quae delectus? Voluptatem, natus possimus voluptate
+        accusamus vitae mollitia dignissimos facilis, recusandae earum, quis
+        saepe corrupti incidunt fuga?
+      </p>
+
+      <button className="btn-primary" >
+        Comienza a explorar las enfermedades
+      </button>
+
+      <button className="btn-primary" >
+        Ver más
+      </button>
+
+      <div className="bg-primary" >primary</div>
+      <div className="bg-secondary" >secondary</div>
+      <div className="bg-danger" >danger</div>
+      <div className="bg-semi-white" >semi-white</div>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
