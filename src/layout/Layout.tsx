d4092e3a449a0361fcc8be6c203807ed74c03214
palcojulian <palcojulian@outlook.com>
2025-04-14 00:47:28 -0500

    feat: :sparkles: Implementa header, footer de acuerdo prototipo figma, se eliminan fichero .css de los componentes, se implementan estilos de tailwind

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,20 +1,22 @@
-import type { JSX } from 'react';
-import Footer from './footer/Footer';
-import Header from './header/Header';
-import './Layout.css';
+import type { JSX } from "react";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
 
 interface Children {
-    children: JSX.Element;
+  children: JSX.Element;
 }
 
+// flex flex-col min-h-screen w-full
+// flex flex-col items-center w-full
+// 
 const Layout = ({ children }: Children) => {
   return (
-    <div className='layout'>
+    <div className="flex flex-col min-h-screen w-full">
       <Header />
-      <main className='layout-content' >{children}</main>
+      <main className="flex flex-col items-center justify-center text-center w-full flex-1">{children}</main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,16 +1,35 @@
-import './Header.css';
-import logo from './../../assets/icons/logo.jpg'
-
+import avatar from "../../../public/img/avatar.png";
+import { NavLink } from "react-router";
+import logoHorizontal from "../../../public/img/logo_horizontal_blanco.png"
 
 const Header = () => {
   return (
-    <header>
-      <nav className='nav'>
-        <img src={logo} alt="" />
-        <h2>Anatolearn</h2>
+    <header className="bg-primary flex items-center justify-between px-8 py-2">
+      <img src={logoHorizontal}  width={120}  />
+      <nav className="flex items-center gap-2 ">
+        <NavLink className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"  to="#" >
+          Inicio
+        </NavLink>
+        <NavLink  className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"  to="#" >
+          Enfermedades
+        </NavLink>
+        <NavLink className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"   to="#" >
+          Quiz interactivo
+        </NavLink>
+        <NavLink  className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1" to="#" >
+          Sobre nosotros
+        </NavLink>
       </nav>
-    </header>
-  )
-}
 
-export default Header
+      <img
+        src={avatar}
+        alt="avatar"
+        height={35}
+        width={35}
+        className="rounded-full object-contain"
+      />
+    </header>
+  );
+};
+
+export default Header;
