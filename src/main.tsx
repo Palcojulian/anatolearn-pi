04eb42e104da1b4871132274a68dc6e209d522cf
palcojulian <palcojulian@outlook.com>
2025-04-15 23:00:59 -0500

    feat: :sparkles: Importan componentes, se definen rutas para cada componente

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import App from './App.tsx';
-import Layout from './layout/Layout.tsx';
-import './assets/css/Main.css'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./assets/css/Main.css";
 
-createRoot(document.getElementById('root')!).render(
- <BrowserRouter>
-  <Layout>
-    <Routes>
-      <Route path='/' element={<App />} />
-    </Routes>
-  </Layout>
- </BrowserRouter>
-)
+import Layout from "./layout/Layout.tsx";
+import Home from "./pages/home/Home.tsx";
+import Diseases from "./pages/heart-diseases/Diseases.tsx";
+import Quiz from "./pages/interactive-quiz/Quiz.tsx";
+import AboutUs from "./pages/about-us/AboutUs.tsx";
+import NotFound from "./pages/not-found/NotFound.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/enfermedades-corazon" element={<Diseases />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
+  </BrowserRouter>
+);
