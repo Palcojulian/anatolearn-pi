2c6c982e78bf0e387f4de5d01d12cfd3762846b1
palcojulian <palcojulian@outlook.com>
2025-04-15 23:03:04 -0500

    feat: :sparkles: Agrega funcionalidad rutas navbar

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,20 +3,22 @@ import { NavLink } from "react-router";
 import logoHorizontal from "../../../public/img/logo_horizontal_blanco.png"
 
 const Header = () => {
+
+  
   return (
     <header className="bg-primary flex items-center justify-between px-8 py-2">
       <img src={logoHorizontal}  width={120}  />
       <nav className="flex items-center gap-2 ">
-        <NavLink className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"  to="#" >
+        <NavLink className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"  to="/" end >
           Inicio
         </NavLink>
-        <NavLink  className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"  to="#" >
+        <NavLink className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"  to="/enfermedades-corazon"  end>
           Enfermedades
         </NavLink>
-        <NavLink className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"   to="#" >
+        <NavLink className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"   to="/quiz" end>
           Quiz interactivo
         </NavLink>
-        <NavLink  className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1" to="#" >
+        <NavLink  className="text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1" to="/about-us" end>
           Sobre nosotros
         </NavLink>
       </nav>
