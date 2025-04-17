8a88c2cb4e612d13614ed796a4ac9143e44bb77c
palcojulian <palcojulian@outlook.com>
2025-04-17 01:27:36 -0500

    feat: :sparkles: Agregan rutas arritmia

--- src/main.tsx
+++ src/main.tsx
@@ -9,14 +9,27 @@ import Quiz from "./pages/interactive-quiz/Quiz.tsx";
 import AboutUs from "./pages/about-us/AboutUs.tsx";
 import NotFound from "./pages/not-found/NotFound.tsx";
 
+/* ARRITMIA */
+import Arrhythmia from "./pages/heart-diseases/arrhythmia/Arrhythmia.tsx";
+import ArrhythmiaWhatIs from "./pages/heart-diseases/arrhythmia/sections/WhatIs.tsx";
+import ArrhythmiaSymptoms from "./pages/heart-diseases/arrhythmia/sections/Symptoms.tsx";
+import ArrhythmiaTreatment from "./pages/heart-diseases/arrhythmia/sections/Treatment.tsx";
+import ArrhythmiaPrevention from "./pages/heart-diseases/arrhythmia/sections/Prevention.tsx";
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Layout>
       <Routes>
         <Route index path="/" element={<Home />} />
-        <Route path="/enfermedades-corazon" element={<Diseases />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="enfermedades-corazon" element={<Diseases />} />
+        <Route path="enfermedades-corazon/arritmia" element={<Arrhythmia />} >
+          <Route path="que-es" element={<ArrhythmiaWhatIs />} />
+          <Route path="sintomas" element={<ArrhythmiaSymptoms />} />
+          <Route path="tratamiento" element={<ArrhythmiaTreatment />} />
+          <Route path="prevencion" element={<ArrhythmiaPrevention />} />
+        </Route>
+        <Route path="quiz" element={<Quiz />} />
+        <Route path="about-us" element={<AboutUs />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
