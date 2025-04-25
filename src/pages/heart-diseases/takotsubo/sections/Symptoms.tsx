a010cdf8c366f0d80d08dbfa55f56332d1b98af7
TiffanyDev <tiffanyasff@gmail.com>
2025-04-24 22:36:00 -0500

    enfermedad de takotsubo

---
+++ src/pages/heart-diseases/takotsubo/sections/Symptoms.tsx
@@ -0,0 +1,18 @@
+const Symptoms = () => {
+  return (
+    <div className="text-justify px-2">
+      <p>
+        Los síntomas son similares a los de un infarto de miocardio e incluyen:
+      </p>
+      <br />
+      <ul>
+        <li>&bull; Dolor repentino en el pecho.</li>
+        <li>&bull; Dificultad para respirar (disnea).</li>
+        <li>&bull; Palpitaciones.</li>
+        <li>&bull; Náuseas o vómitos.</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Symptoms;
