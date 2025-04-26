1d38f6771aabb1447913ba455a1992c6002ee489
palcojulian <palcojulian@outlook.com>
2025-04-26 10:48:25 -0500

    feat: :sparkles: Elimina informacion que no es necesaria del footer, agrega navegacion a la pagina de inicio al realizar una acción sobre el logo de la empresa

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router";
 import logo from "../../../public/img/logo_blanco.png";
 import IconIg from "../../components/IconIg";
 import IconLinkedin from "../../components/IconLinkedin";
@@ -5,9 +6,12 @@ import IconX from "../../components/IconX";
 import IconYt from "../../components/IconYt";
 
 const Footer = () => {
+
+  const navigate = useNavigate();
+
   return (
     <footer className="flex flex-col bg-primary items-center py-3">
-      <div className="flex justify-evenly w-full">
+      <div className="flex justify-evenly w-full hover:cursor-pointer" onClick={() => navigate('/')}>
         <div className="flex flex-col items-center  min-w-[170px]">
           <img src={logo} alt="anatolearn-logo" width={130} />
           <div className="flex justify-between items-center w-full">
@@ -16,27 +20,6 @@ const Footer = () => {
             <IconIg />
             <IconYt />
           </div>
-        </div>
-        <div className="flex flex-col text-white">
-          <h6 className="mb-3">Use cases</h6>
-          <span className="text-sm mb-1 font-light">UX design</span>
-          <span className="text-sm mb-1 font-light">UI design</span>
-          <span className="text-sm mb-1 font-light">Wireframing</span>
-        </div>
-        <div className="flex flex-col text-white">
-          <h6 className="mb-3">Explore</h6>
-          <span className="text-sm mb-1 font-light">Design</span>
-          <span className="text-sm mb-1 font-light">Prototyping</span>
-          <span className="text-sm mb-1 font-light">Development features</span>
-          <span className="text-sm mb-1 font-light">Design systems</span>
-        </div>
-        <div className="flex flex-col text-white">
-          <h6 className="mb-3">Resources</h6>
-          <span className="text-sm mb-1 font-light">Blog</span>
-          <span className="text-sm mb-1 font-light">Best practices</span>
-          <span className="text-sm mb-1 font-light">Colors</span>
-          <span className="text-sm mb-1 font-light">Color wheel</span>
-          <span className="text-sm mb-1 font-light">Support</span>
         </div>
       </div>
       <span className="text-xs text-white">Copyright © 2025 Anatolearn</span>
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,17 +1,18 @@
 import avatar from "../../../public/img/avatar.png";
-import { NavLink, useLocation } from "react-router";
+import { NavLink, useLocation, useNavigate } from "react-router";
 import logoHorizontal from "../../../public/img/logo_horizontal_blanco.png"
+
 
 const Header = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const paths = location.pathname.split("/").filter(Boolean);
-  
   const styleNavLink = "text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1"
   const navSelected = "bg-[#F9F7F7] text-[#3F72AF]";
 
   return (
-    <header className="bg-primary flex items-center justify-between px-8 py-2">
-      <img src={logoHorizontal}  width={120}  />
+    <header className="bg-primary flex items-center justify-between px-8 py-2"  >
+      <img  src={logoHorizontal}  width={120}  onClick={() => navigate('/')} className="hover:cursor-pointer"  />
       <nav className="flex items-center gap-2 ">
         <NavLink className={`${styleNavLink} ${paths[0] == undefined ? navSelected : ''}`}  to="/" end >
           Inicio
