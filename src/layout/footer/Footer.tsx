f7cff619fe0887669aa11e218a342e8ec4218cdb
palcojulian <palcojulian@outlook.com>
2025-04-26 18:57:00 -0500

    refactor: :fire: Comenta iconos redes sociales

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router";
 import logo from "../../../public/img/logo_blanco.png";
-import IconIg from "../../components/IconIg";
-import IconLinkedin from "../../components/IconLinkedin";
-import IconX from "../../components/IconX";
-import IconYt from "../../components/IconYt";
+// import IconIg from "../../components/IconIg";
+// import IconLinkedin from "../../components/IconLinkedin";
+// import IconX from "../../components/IconX";
+// import IconYt from "../../components/IconYt";
 
 const Footer = () => {
 
@@ -15,10 +15,10 @@ const Footer = () => {
         <div className="flex flex-col items-center  min-w-[170px]">
           <img src={logo} alt="anatolearn-logo" width={130} />
           <div className="flex justify-between items-center w-full">
-            <IconX />
+            {/* <IconX />
             <IconLinkedin />
             <IconIg />
-            <IconYt />
+            <IconYt /> */}
           </div>
         </div>
       </div>
