37233e66b4c79a312bb0e9dc842ba02252124364
palcojulian <palcojulian@outlook.com>
2025-04-27 01:02:24 -0500

    feat: :sparkles: Agregan props al componente

--- src/pages/heart-diseases/components/models-3d/Floor.tsx
+++ src/pages/heart-diseases/components/models-3d/Floor.tsx
@@ -1,14 +1,23 @@
-const Floor = () => {
-    return (
-      <mesh
-        rotation-x={-Math.PI / 2}
-        receiveShadow={true}
-        position={[0,-1.3,0]}
-      >
-        <circleGeometry args={[2.5, 50]} />
-        <meshStandardMaterial roughness={0.7} metalness={1} color={'#3F72AF'} />
-      </mesh>
-    );
-  };
-  
-  export default Floor;
+import { Vector3 } from "three";
+
+interface Props {
+  color: string;
+  position: Vector3;
+  metalnesVal: number;
+  roughness: number;
+}
+
+const Floor = (props: Props) => {
+  return (
+    <mesh rotation-x={-Math.PI} receiveShadow={true} position={props.position}>
+      <cylinderGeometry args={[2.5, 2.5, 0.15, 50]} />
+      <meshStandardMaterial
+        roughness={props.roughness}
+        metalness={props.metalnesVal}
+        color={props.color}
+      />
+    </mesh>
+  );
+};
+
+export default Floor;
