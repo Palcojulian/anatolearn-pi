6fb8ef87ac67f6f0dd8e8e38d18a71c29e8929a3
palcojulian <palcojulian@outlook.com>
2025-05-11 23:31:12 -0500

    feat: :sparkles: Declara funcion para traer información de usuario si este existe en el local store, para luego ser guardada en store zustand

--- src/pages/log-in/composables/useAuthUser.ts
+++ src/pages/log-in/composables/useAuthUser.ts
@@ -1,14 +1,21 @@
 import useStoreAuth from "../stores/useStoreAuth"
 import { auth } from '../../../../firebase.config';
 import { onAuthStateChanged, GoogleAuthProvider, signInWithPopup, type User, UserCredential } from 'firebase/auth';
+import { getData, saveData } from '../../../utils/local-store-service';
 
 export function useAuthUser() {
     const { setUserLogged, userLooged } = useStoreAuth();
     
+    const getInfoUserLocalStore = ():void => {
+        const user  = getData('info-user');
+        setUserLogged(user ? JSON.parse(user) : null);
+    }
+
     const loginWithGoogle = async():Promise<void> => {
         const provider = new GoogleAuthProvider();
         try {
             const { user } = await signInWithPopup(auth, provider);
+            saveData('info-user', JSON.stringify(user));        
             setUserLogged(user);
         } catch (error) {  
             console.log(error);
@@ -16,6 +23,7 @@ export function useAuthUser() {
     }
 
     return {
+        getInfoUserLocalStore,
         loginWithGoogle,
         userLooged,
     }
