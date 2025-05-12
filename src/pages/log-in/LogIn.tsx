5e44260ad2cb6e4d6ca9aab391730c69bc398020
palcojulian <palcojulian@outlook.com>
2025-05-11 21:47:01 -0500

    feat: :sparkles: Page inicio de sesionc

---
+++ src/pages/log-in/LogIn.tsx
@@ -0,0 +1,32 @@
+import logo from "../../../public/img/logo_azul.png";
+import logoGoogle from "../../../public/img/google-logo.png";
+import { useAuthUser } from "./composables/useAuthUser";
+import { useCallback } from "react";
+
+const LogIn = () => {
+
+    const { loginWithGoogle, userLooged} = useAuthUser();
+
+    const handleLogIn = useCallback(async() => {
+        await loginWithGoogle();
+    }, [loginWithGoogle])
+
+    return (
+    
+        <div className="flex justify-center w-full" >
+            <div className="flex flex-col gap-3 items-center shadow w-[300px] min-w-[170px] py-7 px-4 rounded">
+                <img src={logo} alt="anatolearn-logo"  width={180} />
+                <span>¡Ingresa con tu cuenta de google!</span>
+                <button onClick={handleLogIn} className="flex border border-gray-100 justify-center gap-2 w-full bg-gray-100 p-1 rounded-[5px] font-medium hover:cursor-pointer hover:border-[#3F72AF] " > 
+                    <img src={logoGoogle} alt="logo-google" width={25} />
+                    Iniciar sesión
+                </button>
+                <div>{userLooged?.email}</div>
+                <img src={userLooged?.photoURL ?? ''} alt="" />
+            </div>
+        </div>
+    )
+}
+
+
+export default LogIn;
