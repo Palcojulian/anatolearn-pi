faf5b8d3e0edbbf3f701c9ae9619038180665dd6
palcojulian <palcojulian@outlook.com>
2025-05-11 21:45:13 -0500

    feat: :sparkles: Tipa variables de entorno

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_FIRE_BASE_API_KEY: string;
+    readonly VITE_FIRE_BASE_AUTH_DOMAIN: string;
+    readonly VITE_FIRE_BASE_PROJECT_ID: string;
+    readonly VITE_FIRE_BASE_STORAGE_BUCKET: string;
+    readonly VITE_FIRE_BASE_MESSAGING_SENDER_ID: string;
+    readonly VITE_FIRE_BASE_APP_ID: string;
+    readonly VITE_FIRE_BASE_MEASUREMENT_ID: string;
+
+}
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
