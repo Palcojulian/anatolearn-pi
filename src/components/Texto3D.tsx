508d1563f7ebd0678d852c355e0bf943c843dc11
palcojulian <palcojulian@outlook.com>
2025-05-13 23:08:14 -0500

    fix: :bug: Elimina useEffect bug en la posicion del texto

--- src/components/Texto3D.tsx
+++ src/components/Texto3D.tsx
@@ -1,5 +1,5 @@
 import { Center, Text3D } from "@react-three/drei";
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 import { Vector3, Mesh, Box3 } from "three";
 
 interface Props {
@@ -17,18 +17,6 @@ interface Props {
 const Texto3D = (props: Props) => {
   const text3DRef = useRef<Mesh>(null);
 
-  useEffect(() => {
-    if (text3DRef.current && props.text) {
-      const box = new Box3().setFromObject(text3DRef.current);
-      const center = new Vector3();
-      box.getCenter(center);
-
-      text3DRef.current.position.x = props.position.x - center.x;
-      text3DRef.current.position.y = props.position.y - center.y;
-      text3DRef.current.position.z = props.position.z - center.z;
-    }
-  }, [text3DRef, props.text]);
-
   return (
     <Center position={props.position}>
       <Text3D
@@ -40,9 +28,10 @@ const Texto3D = (props: Props) => {
         height={props.height}
         size={props.size}
         letterSpacing={props.letterSpacing}
+        castShadow
       >
         {props.text}
-        <meshStandardMaterial color={props.color} />
+        <meshStandardMaterial color={props.color} metalness={0.7} roughness={0.1} />
       </Text3D>
     </Center>
   );
