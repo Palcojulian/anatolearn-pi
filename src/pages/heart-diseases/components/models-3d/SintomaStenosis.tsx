46cad7e02e3a52b3144388148cb42f69871f2622
David <julian.david.rengifo@correounivalle.edu.co>
2025-07-11 16:02:00 -0500

    se agrego puesto en escena y html al segundo modelo de sintomas de stenosis

--- src/pages/heart-diseases/components/models-3d/SintomaStenosis.tsx
+++ src/pages/heart-diseases/components/models-3d/SintomaStenosis.tsx
@@ -1,5 +1,5 @@
 import { useCallback, type JSX } from "react";
-import { useGLTF, useAnimations } from "@react-three/drei";
+import { useGLTF, useAnimations, Html } from "@react-three/drei";
 import { useRef, useEffect, useState } from "react";
 import * as THREE from "three";
 import Texto3D from "../../../../components/Texto3D";
@@ -7,45 +7,58 @@ import useStoreSintomas from "../../arrhythmia/stores/useStoreSintomas";
 import { useMemo } from "react";
 import { Vector3 } from "three";
 
-
-
-
 const SintomaStenosis = (props: JSX.IntrinsicElements["group"]) => {
   const { isAlertText } = useStoreSintomas();
   const positionText = new Vector3(-0.5, 1.4, 0);
   const positionTextAlert = new Vector3(0, 2.5, 1);
   const group = useRef<THREE.Group>(null);
-  const { nodes, materials, animations } = useGLTF('/models-3d/Student.glb');
+  const { nodes, materials, animations } = useGLTF("/models-3d/Student.glb");
   const { actions } = useAnimations(animations, group);
-  const [currentAction, setCurrentAction] = useState("Sick");
-
-
-  
-
+  const [currentAction, setCurrentAction] = useState<string | null>(null);
+  // const [currentAction, setCurrentAction] = useState("Sick");
+  const handleStudent = () => {
+    if (currentAction !== "Dead") {
+      setCurrentAction("Dead");
+    }
+  };
   useEffect(() => {
-    actions[currentAction]?.fadeIn(0.5).play();
+    if (!currentAction || !actions) return;
+
+    Object.values(actions).forEach((action) => {
+      if (action) action.stop();
+    });
+
+    actions[currentAction]?.reset().fadeIn(0.5).play();
+
     return () => {
-    actions[currentAction]?.fadeOut(0.5).play();
+      actions[currentAction]?.fadeOut(0.5);
     };
   }, [actions, currentAction]);
 
-  const handleStudent = useCallback(()=>{
-    setCurrentAction("Dead");
-  }, [])
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.code === "Space") {
+        setCurrentAction("Sick");
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
 
   return (
     <>
       {/* Iluminación */}
       <ambientLight intensity={-3.5} />
 
-      <group ref={group} {...props} dispose={null} onDoubleClick={handleStudent}>
-      
+      <group
+        ref={group}
+        {...props}
+        dispose={null}
+        onDoubleClick={handleStudent}
+      >
         <group name="Scene">
-   
-      
-     
           <group name="Armature">
-        
             <skinnedMesh
               castShadow
               receiveShadow
@@ -53,8 +66,12 @@ const SintomaStenosis = (props: JSX.IntrinsicElements["group"]) => {
               geometry={(nodes.EyeLeft as THREE.SkinnedMesh).geometry}
               material={materials.Wolf3D_Eye}
               skeleton={(nodes.EyeLeft as THREE.SkinnedMesh).skeleton}
-              morphTargetDictionary={(nodes.EyeLeft as THREE.SkinnedMesh).morphTargetDictionary}
-              morphTargetInfluences={(nodes.EyeLeft as THREE.SkinnedMesh).morphTargetInfluences}
+              morphTargetDictionary={
+                (nodes.EyeLeft as THREE.SkinnedMesh).morphTargetDictionary
+              }
+              morphTargetInfluences={
+                (nodes.EyeLeft as THREE.SkinnedMesh).morphTargetInfluences
+              }
             />
             <skinnedMesh
               castShadow
@@ -63,8 +80,12 @@ const SintomaStenosis = (props: JSX.IntrinsicElements["group"]) => {
               geometry={(nodes.EyeRight as THREE.SkinnedMesh).geometry}
               material={materials.Wolf3D_Eye}
               skeleton={(nodes.EyeRight as THREE.SkinnedMesh).skeleton}
-              morphTargetDictionary={(nodes.EyeRight as THREE.SkinnedMesh).morphTargetDictionary}
-              morphTargetInfluences={(nodes.EyeRight as THREE.SkinnedMesh).morphTargetInfluences}
+              morphTargetDictionary={
+                (nodes.EyeRight as THREE.SkinnedMesh).morphTargetDictionary
+              }
+              morphTargetInfluences={
+                (nodes.EyeRight as THREE.SkinnedMesh).morphTargetInfluences
+              }
             />
             <skinnedMesh
               castShadow
@@ -81,24 +102,36 @@ const SintomaStenosis = (props: JSX.IntrinsicElements["group"]) => {
               geometry={(nodes.Wolf3D_Head as THREE.SkinnedMesh).geometry}
               material={materials.Wolf3D_Skin}
               skeleton={(nodes.Wolf3D_Head as THREE.SkinnedMesh).skeleton}
-              morphTargetDictionary={(nodes.Wolf3D_Head as THREE.SkinnedMesh).morphTargetDictionary}
-              morphTargetInfluences={(nodes.Wolf3D_Head as THREE.SkinnedMesh).morphTargetInfluences}
+              morphTargetDictionary={
+                (nodes.Wolf3D_Head as THREE.SkinnedMesh).morphTargetDictionary
+              }
+              morphTargetInfluences={
+                (nodes.Wolf3D_Head as THREE.SkinnedMesh).morphTargetInfluences
+              }
             />
             <skinnedMesh
               castShadow
               receiveShadow
               name="Wolf3D_Outfit_Bottom"
-              geometry={(nodes.Wolf3D_Outfit_Bottom as THREE.SkinnedMesh).geometry}
+              geometry={
+                (nodes.Wolf3D_Outfit_Bottom as THREE.SkinnedMesh).geometry
+              }
               material={materials.Wolf3D_Outfit_Bottom}
-              skeleton={(nodes.Wolf3D_Outfit_Bottom as THREE.SkinnedMesh).skeleton}
+              skeleton={
+                (nodes.Wolf3D_Outfit_Bottom as THREE.SkinnedMesh).skeleton
+              }
             />
             <skinnedMesh
               castShadow
               receiveShadow
               name="Wolf3D_Outfit_Footwear"
-              geometry={(nodes.Wolf3D_Outfit_Footwear as THREE.SkinnedMesh).geometry}
+              geometry={
+                (nodes.Wolf3D_Outfit_Footwear as THREE.SkinnedMesh).geometry
+              }
               material={materials.Wolf3D_Outfit_Footwear}
-              skeleton={(nodes.Wolf3D_Outfit_Footwear as THREE.SkinnedMesh).skeleton}
+              skeleton={
+                (nodes.Wolf3D_Outfit_Footwear as THREE.SkinnedMesh).skeleton
+              }
             />
             <skinnedMesh
               castShadow
@@ -115,14 +148,38 @@ const SintomaStenosis = (props: JSX.IntrinsicElements["group"]) => {
               geometry={(nodes.Wolf3D_Teeth as THREE.SkinnedMesh).geometry}
               material={materials.Wolf3D_Teeth}
               skeleton={(nodes.Wolf3D_Teeth as THREE.SkinnedMesh).skeleton}
-              morphTargetDictionary={(nodes.Wolf3D_Teeth as THREE.SkinnedMesh).morphTargetDictionary}
-              morphTargetInfluences={(nodes.Wolf3D_Teeth as THREE.SkinnedMesh).morphTargetInfluences}
+              morphTargetDictionary={
+                (nodes.Wolf3D_Teeth as THREE.SkinnedMesh).morphTargetDictionary
+              }
+              morphTargetInfluences={
+                (nodes.Wolf3D_Teeth as THREE.SkinnedMesh).morphTargetInfluences
+              }
             />
             <primitive object={nodes.Hips} />
-     
           </group>
         </group>
       </group>
+      {/* Controles */}
+      <Html position={[-2, 0, 2]} center>
+        <div
+          style={{
+            backgroundColor: "rgba(0,0,0,0.8)",
+            color: "white",
+            padding: "15px",
+            borderRadius: "8px",
+            fontSize: "12px",
+            maxWidth: "200px",
+            boxShadow: "0 4px 8px rgba(0,0,0,0.5)",
+          }}
+        >
+          <h3 style={{ margin: "0 0 10px 0", fontSize: "14px" }}>Controles:</h3>
+          <ul style={{ margin: 0, paddingLeft: "15px" }}>
+            <li>Tecla Espacio: Animacion mareo.</li>
+            <li>-------------</li>
+            <li>Doble Click Derecho al Mouse: Animación desmayo.</li>
+          </ul>
+        </div>
+      </Html>
     </>
   );
 };
