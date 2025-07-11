3ee5050b7b4f501e944052bacc9b5b64f16470f9
palcojulian <palcojulian@outlook.com>
2025-07-10 20:37:59 -0500

    feat: :sparkles: Agrega contenedor padre, y se alinea a la derecha

--- src/components/BtnLogIn.tsx
+++ src/components/BtnLogIn.tsx
@@ -6,12 +6,14 @@ const BtnLogIn = () => {
     "text-semi-white text-base font-medium hover:bg-[#F9F7F7] hover:text-[#3F72AF] rounded-md px-2 py-1";
 
   return (
-    <button
-      className={`${styleNavLink} border hover:cursor-pointer`}
-      onClick={() => navigate("/iniciar-sesion")}
-    >
-      Iniciar sesión
-    </button>
+    <div className="flex justify-end items-center" >
+      <button
+        className={`${styleNavLink} border w-[140px] hover:cursor-pointer`}
+        onClick={() => navigate("/iniciar-sesion")}
+      >
+        Iniciar sesión
+      </button>
+    </div>
   );
 };
 
